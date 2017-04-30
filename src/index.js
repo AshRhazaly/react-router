6ba970984e28269084cb0eca03eddec1b9f0c0b6
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-04-30 21:00:21 +0800

    Fix bug with multiple routes not rendering

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 import {
   HashRouter,
   Route,
-  Link
+  Link,
 } from 'react-router-dom';
 
 // CSS
@@ -13,13 +13,16 @@ import './css/style.css';
 // Components
 import App from './components/App';
 import Home from './components/Home';
-import About from './components/About'
-import Courses from './components/Courses'
-import Teachers from './components/Teachers'
+import About from './components/About';
+import Courses from './components/Courses';
+import Teachers from './components/Teachers';
 
 // Render
 render((
   <HashRouter>
-    <Route path="/" component={Home} />
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </div>
   </HashRouter>
 ), document.getElementById('root'));
