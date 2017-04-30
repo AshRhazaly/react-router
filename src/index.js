1b3264700b90a8e0b61719431c755d5e485d8327
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-04-30 20:02:43 +0800

    Install react-router-dom for hashhistory

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import './css/style.css';
 
 // Components
 import App from './components/App';
+import Home from './components/Home';
 
 // Render
 render((
   <Router history={hashHistory}>
     <Route path="/" component={Home} />
   </Router>
-), document.getElementById('root')
-);
+), document.getElementById('root'));
