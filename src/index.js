81c07b689b28c0692052e421e053c63c1bc1538e
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-04-30 19:43:55 +0800

    Import router and route in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // Libs
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route } from 'react-router';
 
-// CSS 
+// CSS
 import './css/style.css';
 
 // Components
